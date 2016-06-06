2a5a8d84f6c8e4f4f80b515d9ebf6940a10ee15c
enjoy1358 <enjoy1358@nate.com>
2016-06-06 23:07:08 +0900

    홈페이지 URL 추가

--- diarymemo/src/com/ybproject/diarymemo/desk/DiaryMemoHomepage.java
+++ diarymemo/src/com/ybproject/diarymemo/desk/DiaryMemoHomepage.java
@@ -17,7 +17,8 @@ public class DiaryMemoHomepage extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		Uri uri = Uri.parse("");
+		Uri uri = Uri.parse("http://skin-skin2.enjoy1358.cafe24.com");   // 저희가 직접 만든
+																		 // 홈페이지로 이동합니다.
 		Intent it = new Intent(Intent.ACTION_VIEW,uri);
 		startActivity(it);
 		finish();
